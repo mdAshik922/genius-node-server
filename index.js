5cfc5c9d59d076b357df6267688a5d62cc1dc7b3
mdAshik922 <md.ashik0005723@gmail.com>
2021-10-27 21:54:44 +0600

    commit

--- index.js
+++ index.js
@@ -8,43 +8,56 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89jki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const { MongoClient, LEGAL_TCP_SOCKET_OPTIONS } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0g3ft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("Food_Master").collection("user");
 
-//   app.get('/users', (req, res) =>{
-//       const users = collection.find({});
-//       const allUsers = users.toArray();
-//       res.send(users) })
-//   // perform actions on the collection object
-// //   client.close();
-// console.log('db conect')
-// });
 async function run() {
     try {
       await client.connect();
-      const database = client.db('Food_Master');
-      const movies = database.collection('user');
+      const database = client.db('genius_car');
+      const serviceCollection = database.collection('services');
+
       app.get('/users', async(req, res) =>{
-              const users = collection.find({});
-              const allUsers = users.toArray();
-              res.send(users)
+              const users = serviceCollection.find({});
+              const allUsers = await users.toArray();
+              res.send(allUsers)
           })
 
+// GET SINGLE SERVICE
+app.get('/service/:id', async(req, res) =>{
+  const id = req.params.id;
+  console.log('getting specific id', id);
+  const query = {_id: ObjectId(id)};
+  const service = await serviceCollection.findOne(query);
+  res.json(service)
+})
+//GET POST API
           app.post('/service', async(req, res)=>{
-            const users = collection.find({});
-            const allUsers = users.toArray();
-            res.json(users)
-
+            const service = req.body;
+            const allUsers = await serviceCollection.insertOne(service);
+            res.json(allUsers)
           })
-          console.log('db conect moto');
+
+          //GET DELETE API
+          app.delete('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            // console.log(id)
+            const query = {_id: ObjectId(id)}
+            const result = await serviceCollection.deleteOne(query)
+            res.json(result);
+          })
+
+          // console.log('db conect ');
     } finally {
       // Ensures that the client will close when you finish/error
     //   await client.close();
     }}
   run().catch(console.dir);
+  
+  app.get('/', async(req, res) =>(
+    res.send('hello world')
+  ))
 
 app.listen(port, ()=>{
     console.log('my server running', port)
