3823ce7475739292724e7c65a9ef2b589bb97768
mdAshik922 <md.ashik0005723@gmail.com>
2021-10-27 07:05:27 +0600

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = (express());
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 app.use(cors());
